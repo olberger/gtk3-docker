d2a4daa810790009e01e245d4ab79b0a730978c2
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2018-01-29 17:28:43 +0100

    Make sure the message is actually displayed

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ docker rm wireshark-broadway
 # If needed, we can change Debian base version
 #DEBIAN_VERSION=testing
 
-echo <<EOF
 # stable-slim is better than stable, as it produces image 290MB vs 382MB
 DEBIAN_VERSION=stable-slim
 DEB_VER=$(docker run --rm -it debian:$DEBIAN_VERSION cat /etc/debian_version | sed 's/\r$//')
@@ -15,6 +14,8 @@ echo "Building for Debian $DEBIAN_VERSION $DEB_VER"
 
 # Actually passing build-arg variable debian_version may not be necessary of equal to default (stable)
 docker build --build-arg debian_version=$DEBIAN_VERSION -t wireshark-broadway:debian-$DEB_VER .
+
+cat <<EOF
 to run, do : 
 
 docker run -d --rm -it -p 8085:8085 --name wireshark-broadway wireshark-broadway:debian-$DEB_VER
